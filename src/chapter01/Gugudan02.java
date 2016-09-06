eb9da9004df4c5f857a47d16644ba7a217c4c59e
BITUSER <BITUSER@BITPC>
2016-09-06 15:36:12 +0900

    homework 4ea

---
+++ src/chapter01/Gugudan02.java
@@ -0,0 +1,16 @@
+package chapter01;
+
+public class Gugudan02 {
+
+	public static void main(String[] args) {
+		for(int i = 2; i <= 9; i++){
+			for(int j = 1; j <= 9; j++){
+				int temp = i * j;
+				System.out.println(i + "*" + j + "=" + temp);
+			}
+								
+		}
+
+	}
+
+}
